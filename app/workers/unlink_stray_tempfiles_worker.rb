9f02586fd70626e28b471410a811f63ed8500ed0
Ben McFadden <ben@forgeapps.com>
2016-05-07 14:43:41 -0700

    Add unlink stray tempfile worker

---
+++ app/workers/unlink_stray_tempfiles_worker.rb
@@ -0,0 +1,52 @@
+class UnlinkStrayTempfilesWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false
+  
+  REMOVE_AFTER = 20.minutes
+  FILE_LIMIT = 500
+  
+  def perform
+    path = Rails.root.join('tmp').join('camera-event-assets')
+    find_and_unlink_old_files_in_path(path)
+  end
+
+  def find_and_unlink_old_files_in_path(path)
+    Dir.glob("#{path}/*/").first(FILE_LIMIT).each do |directory_path|
+      if File.directory?(directory_path)
+        # This is a directory
+        should_delete_directory = true
+        Dir.glob("#{directory_path}/*").each do |file_path|
+          if !unlink_file_if_old(file_path)
+            should_delete_directory = false
+          end
+        end
+      
+        if should_delete_directory
+          begin
+            Dir.unlink(directory_path)
+          rescue SystemCallError
+            # The directory probably isn't empty. Let's just ignore that for now.
+            nil
+          end
+        end
+      else
+        # This is a file.. not sure why there's a file loose in here.
+        # Just ignore it I guess.
+        nil
+      end
+    end
+  end
+
+  def unlink_file_if_old(file_path)
+    if File.ctime(file_path) < (Time.now - REMOVE_AFTER)
+      # We're old!
+    
+      Rails.logger.debug("Unlinking #{file_path}")
+      File.unlink(file_path)
+      return true
+    else
+      return false
+    end
+  end
+end
--- clock.rb
+++ clock.rb
@@ -5,27 +5,7 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  handler do |job|
-    puts "Running #{job}"
-
-    if job == 'minutely.job'
-      Camera.all.each(&:find_and_process_new_motion_events)
-    elsif job == 'hourly.job'
-      CameraEvent.purge_old_events!
-    end
-
-  end
-
-  # handler receives the time when job is prepared to run in the 2nd argument
-  # handler do |job, time|
-  #   puts "Running #{job}, at #{time}"
-  # end
-
-  every(1.minutes, 'minutely.job')
-  every(1.hour, 'hourly.job')
-  #every(10.minutes, 'ten-minutely.job')
-  # every(30.minutes, 'GeocodeAnalyticsEvents')
-  #
-  # every(1.hour, 'MorningSystemStatusCheck', at: '15:00') # 8am MST
-  # every(5.minutes, 'SystemStatusCheck')
+  every(10.minutes, 'tempfiles.clean') { UnlinkStrayTempfilesWorker.perform_async }
+  every(1.minute, 'camera.find_new_motion_events'){ Camera.all.each(&:find_and_process_new_motion_events) }
+  every(1.hour, 'camera_event.purge_old_events'){ CameraEvent.purge_old_events! }
 end
