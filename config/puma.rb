da20bec28cb804cdec67de58dedb56a1fe6bd946
Ben McFadden <ben@ministrycentered.com>
2017-01-25 15:11:35 -0700

    Move puma socket location

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ rackup      DefaultRackup
 environment ENV['RACK_ENV'] || 'development'
 
 directory '/www/greateye/current'
-bind "unix:///www/greateye/current/tmp/sockets/puma.greateye.sock"
+bind "unix:///www/greateye/current/tmp/sockets/greateye.sock"
 state_path "/www/greateye/current/tmp/pids/puma.state"
 pidfile "/www/greateye/current/tmp/pids/puma.pid"
 stdout_redirect '/www/greateye/current/log/puma.stdout.log', '/www/greateye/current/log/puma.stderr.log', true
