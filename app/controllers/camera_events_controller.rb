fd22b8560b59b6b2c877d25c1459c54c5d090d10
Ben McFadden <ben@ministrycentered.com>
2017-01-26 20:40:12 -0700

    Fix linking to event inline

--- app/controllers/camera_events_controller.rb
+++ app/controllers/camera_events_controller.rb
@@ -2,24 +2,24 @@ class CameraEventsController < ApplicationController
   def index
     if params[:show_event_id]
       event = CameraEvent.find(params[:show_event_id])
-      offset = CameraEvent.complete.ordered.where("created_at > ?", event.created_at).count
+      offset = CameraEvent.complete.ordered.where("event_timestamp > ?", event.event_timestamp).count
       page = (offset / 24) + 1
       params[:page] = page
     end
-    
+
     @events = CameraEvent.complete.ordered.page(params[:page]).per(24)
   end
-  
+
   def kept
     @events = CameraEvent.complete.kept.ordered.page(params[:page]).per(24)
   end
-  
+
   def keep
     @event = CameraEvent.find(params[:id])
     @event.keep!
     redirect_to :back
   end
-  
+
   def unkeep
     @event = CameraEvent.find(params[:id])
     @event.unkeep!
