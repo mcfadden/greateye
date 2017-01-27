d5b85422c0e6bd70eafef875a2f92b9a8ae26e99
Ben McFadden <ben@ministrycentered.com>
2017-01-26 20:38:07 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Camera.count == 0
+  Camera.create(name: "Driveway", model: 0, active: true)
+  Camera.create(name: "Porch", model: 0, active: true)
+  Camera.create(name: "Back Yard", model: 0, active: true)
+  Camera.create(name: "Front Yard", model: 0, active: true)
+end
+
+durations = []
+100.times{durations << 35}
+25.times{durations << rand(200) + 30}
+
+Camera.all.each do |camera|
+  time_offset = Time.now
+  100..(rand(500) + 300).times do |i|
+    time_offset -= (rand(5000) + 1000).seconds
+    event = camera.camera_events.create(status: :complete, duration: durations.sample, event_timestamp: time_offset)
+
+    event.camera_event_assets.create(asset_type: 'image/jpeg', status: :complete)
+    event.camera_event_assets.create(asset_type: 'video/mp4', status: :complete)
+  end
+end
