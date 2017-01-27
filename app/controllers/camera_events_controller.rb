bf34f92c2d05ec2890e1bfb6db89e8d86208c698
Ben McFadden <ben@ministrycentered.com>
2017-01-26 21:38:34 -0700

    I've had too much to drink to guess this

--- app/controllers/camera_events_controller.rb
+++ app/controllers/camera_events_controller.rb
@@ -11,7 +11,7 @@ class CameraEventsController < ApplicationController
       @camera_event_timeline = []
       CameraEvent.includes(:camera).where("event_timestamp > ?", 30.hours.ago).each do |ce|
         next if ce.duration > 1.hour
-        @camera_event_timeline << [ce.camera.name, ce.event_timestamp - 7.hours, ce.event_timestamp + ce.duration - 7.hours]
+        @camera_event_timeline << [ce.camera.name, ce.event_timestamp + 7.hours, ce.event_timestamp + ce.duration + 7.hours]
       end
     end
 
