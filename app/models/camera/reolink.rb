68281f44e2fb0c46b707a5fb145c05ed2139c079
Ben McFadden <ben@ministrycentered.com>
2017-03-27 20:40:29 -0700

    Preview URL for reolink 410

--- app/models/camera/reolink.rb
+++ app/models/camera/reolink.rb
@@ -1,3 +1,12 @@
 class Camera::Reolink < Camera
   include Concerns::CameraInteractionMethod::Ftp
+
+  def video_events?
+    true
+  end
+
+  def preview_url
+    "http://#{host}/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=abc#{rand(12)}&user=#{username}&password=#{password}"
+  end
+
 end
