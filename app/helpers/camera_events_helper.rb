6a5a9a7b698a48d48c8042ffa4a0a31a7baa905a
Ben McFadden <ben@ministrycentered.com>
2017-04-17 21:02:44 -0700

    Dynamic columns in index

--- app/helpers/camera_events_helper.rb
+++ app/helpers/camera_events_helper.rb
@@ -1,2 +1,26 @@
 module CameraEventsHelper
+  def column_classes_for_index
+    classes = []
+    case SystemSetting.columns_in_listing_view
+    when 1
+      classes << 'col-sm-12'
+      classes << 'col-md-12'
+    when 2
+      classes << 'col-sm-12'
+      classes << 'col-md-6'
+    when 3
+      classes << 'col-sm-6'
+      classes << 'col-md-4'
+    when 4
+      classes << 'col-sm-6'
+      classes << 'col-md-3'
+    when 5..6
+      classes << 'col-sm-4'
+      classes << 'col-md-2'
+    else
+      classes << 'col-sm-2'
+      classes << 'col-md-1'
+    end
+    classes
+  end
 end
