a649e8e423c861df167e441fa467bc2687ca5d9c
Ben McFadden <ben@ministrycentered.com>
2017-07-13 15:50:43 -0700

    prompt for git branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@bitbucket.org:benmcfadden/greateye.git'
 # set :rbenv_roles, :all # default value
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/www/greateye'
