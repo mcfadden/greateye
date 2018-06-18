42d6702953c2741fef80ec2f280582ef5de5203e
Ben McFadden <ben@ministrycentered.com>
2018-06-18 16:35:14 -0700

    Summary information per day

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,5 +1,13 @@
 class Admin::StatsController < Admin::BaseController
   def index
     @storage_per_camera = CameraEventAsset.joins(:camera_event).group('camera_events.camera_id').sum(:asset_size)
+    first_event_timestamp = CameraEvent.ordered.unkept.where.not(event_timestamp: nil).first.event_timestamp || 30.days.ago
+    @grouped_events = CameraEvent.group_by_day(:event_timestamp, range: (first_event_timestamp..Time.now), format: "%m-%d").count
+    @grouped_event_storage = CameraEventAsset
+      .joins(:camera_event)
+      .group_by_day(:event_timestamp, range: (first_event_timestamp..Time.now), format: "%m-%d")
+      .sum(:asset_size)
+      .map{ |k, v| [k, (v.to_f / 1.megabyte).round]}
+      .to_h
   end
 end
