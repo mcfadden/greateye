eccf0440305ffa305f0dd933d63daa695a56f896
Ben McFadden <ben@ministrycentered.com>
2018-06-18 16:41:31 -0700

    Fix date range filter on graphs

--- app/controllers/admin/cameras/stats_controller.rb
+++ app/controllers/admin/cameras/stats_controller.rb
@@ -1,8 +1,8 @@
 class Admin::Cameras::StatsController < Admin::BaseController
   def show
     @camera = Camera.find(params[:camera_id])
-    first_event_timestamp = @camera.camera_events.ordered.unkept.first.event_timestamp || 30.days.ago
-    @grouped_events = @camera.camera_events.group_by_day(:event_timestamp, range: (first_event_timestamp..Time.now), format: "%m-%d").count
+    oldest_event_timestamp = CameraEvent.ordered.unkept.where.not(event_timestamp: nil).last.event_timestamp || 30.days.ago
+    @grouped_events = @camera.camera_events.group_by_day(:event_timestamp, range: (oldest_event_timestamp..Time.now), format: "%m-%d").count
     @grouped_event_storage = CameraEventAsset
       .joins(:camera_event)
       .where(camera_events: { camera_id: @camera.id })
--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,8 +1,8 @@
 class Admin::StatsController < Admin::BaseController
   def index
     @storage_per_camera = CameraEventAsset.joins(:camera_event).group('camera_events.camera_id').sum(:asset_size)
-    first_event_timestamp = CameraEvent.ordered.unkept.where.not(event_timestamp: nil).first.event_timestamp || 30.days.ago
-    @grouped_events = CameraEvent.group_by_day(:event_timestamp, range: (first_event_timestamp..Time.now), format: "%m-%d").count
+    oldest_event_timestamp = CameraEvent.ordered.unkept.where.not(event_timestamp: nil).last.event_timestamp || 30.days.ago
+    @grouped_events = CameraEvent.group_by_day(:event_timestamp, range: (oldest_event_timestamp..Time.now), format: "%m-%d").count
     @grouped_event_storage = CameraEventAsset
       .joins(:camera_event)
       .group_by_day(:event_timestamp, range: (first_event_timestamp..Time.now), format: "%m-%d")
