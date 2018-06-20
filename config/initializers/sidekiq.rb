74b5218a278c7b2a858f65a773d1a099d9326f22
Ben McFadden <ben@ministrycentered.com>
2018-06-20 13:36:17 -0700

    Allow database and redis hosts to be configured

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'great-eye' }
+  config.redis = { host: ENV['REDIS_HOST'] || 'localhost', namespace: 'great-eye' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'great-eye', size: 1}
+  config.redis = { host: ENV['REDIS_HOST'] || 'localhost', namespace: 'great-eye', size: 1}
 end
